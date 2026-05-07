2c41042a18b08cc4e7f97eefed8a1572de3798bb
Michelle_C <michellecarrazco70@gmail.com>
2026-05-06 21:06:38 -0700

    Add files via upload

---
+++ Login_EnergySaver/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Login_EnergySaver.Models;
+
+namespace Login_EnergySaver.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
