15159718d4daa704f063f5682d373f714fb86286
Lizbeth Valencia <L23600094@huatabampo.tecnm.mx>
2026-05-10 23:25:43 -0700

    Se actualizacion botones y menú

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using EnergySaver.Data;
+using EnergySaver.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+public class AccountController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public AccountController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string correo, string password)
+    {
+        var user = _context.Usuarios
+            .FirstOrDefault(u => u.Correo == correo && u.Password == password);
+
+        if (user != null)
+        {
+            if (user.Rol == "Admin")
+                return RedirectToAction("Dashboard", "Admin");
+            else
+                return RedirectToAction("Usuario", "Home");
+        }
+
+        ViewBag.Error = "Datos incorrectos";
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(Usuario u)
+    {
+        _context.Usuarios.Add(u);
+        _context.SaveChanges();
+
+        TempData["Mensaje"] = "Registro exitoso ✅";
+
+        return RedirectToAction("Login");
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EnergySaver.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnergySaver.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
